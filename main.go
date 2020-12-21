8171340d2f0143b64e664d126a9c97e2a95652a2
linkc0829 <66521451+linkc0829@users.noreply.github.com>
2020-12-22 04:02:33 +0800

    init

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+// HelloWorld for hello world
+func HelloWorld() string {
+	return "Hello World, drone workshop!"
+}
+
+func main() {
+	fmt.Println(HelloWorld())
+}
